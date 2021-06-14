5f6fb875c0063d8bfd988cbad8b7dd94fa103d1d
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-06-14 13:47:49 -0400

    Annotate functions

--- pycamera/frame.py
+++ pycamera/frame.py
@@ -7,13 +7,20 @@ class Frame:
         self.frame = frame
 
     def to_numpy(self) -> np.ndarray:
+        """Returns raw NumPy array"""
         return self.frame
 
     def rgb(self):
-        return cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+        """Converts BGR frame to RGB"""
+        return Frame(cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB))
+
+    def pillow(self):
+        """Returns a PIL (Pillow) compatible image. Alias of rgb()"""
+        return self.rgb()
 
     def show(self):
         cv2.imshow("pycamera frame", self.frame)
 
     def save(self, fp: str):
+        """Writes image to file"""
         cv2.imwrite(fp, self.frame)
--- tests/main.py
+++ tests/main.py
@@ -1,14 +1,7 @@
 from pycamera import camera
-import pycamera
-import cv2
-import time
 
-pycamera.disable_logging()
-cam = camera.Camera(0)
+cam = camera.Camera(1)
 
-i = 0
-while True:
+for i in range(5):
     snap = cam.snap()
     snap.save(f"output-{i}.jpg")
-    time.sleep(1)
-    i += 1
