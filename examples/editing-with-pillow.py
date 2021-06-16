3489a8abd05d6f708c54c1ce856a1fd3e91aa2a4
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-06-16 17:11:59 -0400

    Add rectangle to editing-with-pillow example

--- examples/editing-with-pillow.py
+++ examples/editing-with-pillow.py
@@ -8,4 +8,5 @@ image = snap.to_pillow()  # Convert pycamera image to Pillow image
 draw = ImageDraw.Draw(image)
 white = (255, 255, 255)
 draw.ellipse([(10, 10), (100, 100)], width=3, fill=white, outline=white)  # Draw circle
+draw.rectangle([(140, 10), (240, 100)], fill=white, outline=white, width=3)
 image.show()
